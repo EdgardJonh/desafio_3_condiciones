e83f257509f7d674c06b0dd6dfe66728ca94dc7c
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-02-10 22:36:57 -0300

    desafio 3 condicionales

---
+++ assets/js/scripts.js
@@ -0,0 +1,49 @@
+// // let div1 = document.querySelector('.divs')
+ 
+
+// // div1.addEventListener('click', function () {
+   
+// //      let cot = div1.classList.contains('border')
+// //     if (cot == false) {
+// //         div1.classList.add('border')
+// //     } else if(cot == true){
+// //         div1.classList.remove('border')
+// //     }
+// // })
+
+
+// // let mensaje = document.querySelector('.mensaje')
+// const verificar = document.getElementById('verificar')
+
+// const t1 = document.getElementById('t1').value
+// // // let t2 = document.querySelector('#t2')
+// // // let t3 = document.querySelector('#t3')
+ 
+// const elt1 = Number.parseInt(t1)
+// // // let elt2 = Number(t2)
+// // // let elt3 = Number(t3)
+// // // total = elt1 + elt2 + elt3
+
+// // // verificar.addEventListener('click', function () {
+// // //     if (total >10) {
+// // //         console.log('llevas muchos ticket')
+// // //     } else {
+// // //         console.log('llevas'+total)
+// // //     }
+// // // })
+
+// verificar.addEventListener("click", cal)
+
+// function cal() {
+//     console.log(elt1)
+// }
+
+// // devisible el resto que no tenga resto
+
+// let edad = Number(prompt('ingresa edad'))
+
+// if () {
+    
+// }
+
+
